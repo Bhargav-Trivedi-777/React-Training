45b891f4d5d4512bcbfa2cfd320c76c736749dc2
Bhargav <bhargav.trivedi@streebo.com>
2025-09-23 17:14:44 +0530

    fix UI issue and change the abount content

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,12 +6,14 @@ export default function Alert(props) {
         return lower.charAt(0).toUpperCase() +lower.slice(1);
     }
     return (
-        props.alert &&
+        <div style={{height:"50px"}}>
+        {props.alert &&
         <div className='container'>
             <div className={`alert alert-${props.alert.Type} alert-dismissible fade show`} role="alert">
                 <strong>{capitalize(props.alert.Type)}</strong> : {props.alert.Msg}
                 <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
+        </div>}
         </div>
     )
 }
--- src/components/Utils.js
+++ src/components/Utils.js
@@ -21,7 +21,7 @@ export default function TextForm(prop) {
         prop.showAlert("Converted to Lowercase", "success")
     }
     const ClaerText = () => {
-        if(text ===""){
+        if (text === "") {
             prop.showAlert("Text box already empty", "danger");
             return;
         }
@@ -42,7 +42,7 @@ export default function TextForm(prop) {
         words.forEach(item => {
             big = (big == null || big.length < item.length) ? item : big;
         })
-        prop.showAlert("Longest word is : " + big , "primary")
+        prop.showAlert("Longest word is : " + big, "primary")
     }
     const Speak = () => {
         const utterance = new SpeechSynthesisUtterance(text);
@@ -66,17 +66,17 @@ export default function TextForm(prop) {
                                 color: prop.darkMode ? "white" : "black"
                             }} />
                     </div>
-                    <div className="btn btn-primary mx-1" onClick={HandleUpKey}>Convert to Uppercase</div>
-                    <div className="btn btn-primary mx-1 " onClick={HandleLoKey}>Convert to Lowecase</div>
-                    <div className="btn btn-primary mx-1 " onClick={LongWord}>Longest Word Length</div>
-                    <div className="btn btn-primary mx-1 " onClick={Speak}>Speak</div>
-                    <div className="btn btn-success mx-1 " onClick={CopyToClipboard}>Copy</div>
-                    <div className="btn btn-warning mx-1 " onClick={ClaerText}>Clear</div>
+                    <div className="btn btn-primary mx-1 my-1" onClick={HandleUpKey}>Convert to Uppercase</div>
+                    <div className="btn btn-primary mx-1 my-1" onClick={HandleLoKey}>Convert to Lowecase</div>
+                    <div className="btn btn-primary mx-1 my-1" onClick={LongWord}>Longest Word Length</div>
+                    <div className="btn btn-primary mx-1 my-1" onClick={Speak}>Speak</div>
+                    <div className="btn btn-success mx-1 my-1" onClick={CopyToClipboard}>Copy</div>
+                    <div className="btn btn-warning mx-1 my-1" onClick={ClaerText}>Clear</div>
                 </div>
                 <div className='container my-3'>
                     <h2 className='my-3'>Your Text Summary</h2>
-                    <p> {text === "" ? 0 : text.split(" ").length} words and {text.length} characters</p>
-                    <p> {text === "" ? 0 : 0.008 * text.split(" ").length } Minutes read</p>
+                    <p> {text.split(" ").filter((element) => { return element.length !== 0 }).length} words and {text.length} characters</p>
+                    <p> {text === "" ? 0 : 0.008 * text.split(" ").length} Minutes read</p>
                     <h2 className='my-3'>Preview</h2>
                     <p>{text}</p>
                 </div>
@@ -84,19 +84,19 @@ export default function TextForm(prop) {
                     <h2>Find and Replace</h2>
                     <div className="mb-3">
                         <h4>Find</h4>
-                        <input type="text" name="find" id="findBox" onChange={(e) => setFind(e.target.value)} value={find} 
-                        style={{
+                        <input type="text" name="find" id="findBox" onChange={(e) => setFind(e.target.value)} value={find}
+                            style={{
                                 backgroundColor: prop.darkMode ? "#b5888870" : "#8cd2dd44",
                                 color: prop.darkMode ? "white" : "black"
                             }} />
                     </div>
                     <div className="mb-3">
                         <h4>Replace</h4>
-                        <input type="text" name="replace" id="replaceBox" onChange={(e) => setReplace(e.target.value)} value={repalce} 
-                        style={{
+                        <input type="text" name="replace" id="replaceBox" onChange={(e) => setReplace(e.target.value)} value={repalce}
+                            style={{
                                 backgroundColor: prop.darkMode ? "#b5888870" : "#8cd2dd44",
                                 color: prop.darkMode ? "white" : "black"
-                        }} />
+                            }} />
                     </div>
                     <div className="btn btn-primary mx-1" onClick={FindAndReplace} >Find and replace</div>
                 </div>
